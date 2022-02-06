ec4be5ed20e134e0dbf5e0ce990f7eb7dfab6254
aaronWu <aaron.wu@kiwigame.com>
2022-02-06 19:53:40 +0800

    updated plugins vuetify.js

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,14 +1,10 @@
 import Vue from 'vue'
 import '@mdi/font/css/materialdesignicons.css'
-import { MdiMenuOpen } from '@mdi/font/css/materialdesignicons.css'
 import Vuetify from 'vuetify/lib/framework'
 
 Vue.use(Vuetify);
 
-Vue.component('mdi-menu-open', MdiMenuOpen)
 
 export default new Vuetify({
-  icons: {
-    iconfont: 'mdi', // 默认值 - 仅用于展示目的
-  },
+
 });
